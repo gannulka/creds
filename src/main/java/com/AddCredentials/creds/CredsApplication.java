8f52db5e25bc0ff94621118b3c5a4d0bd0c780be
Gannulka <annachulets@gmail.com>
2019-04-22 23:48:36 +0300

    Initial commit

---
+++ src/main/java/com/AddCredentials/creds/CredsApplication.java
@@ -0,0 +1,13 @@
+package com.AddCredentials.creds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AddCredentials/creds/CredsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.AddCredentials.creds;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CredsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
